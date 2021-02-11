9c9d1ed0f46c6450f063b6286375d712f974b71e
dannythedeveloper1 <daniel.girma9@gmail.com>
2021-02-11 13:14:56 -0500

    facebookheader

---
+++ fb-clone/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./Header";
+function App() {
+	return (
+		<div className="app">
+      <Header/>
+      {/* App body */}
+      {/* sidebar */}
+      {/* feed */}
+      {/* widgets */}
+		</div>
+	);
+}
+
+export default App;
